09f8a8102ca4e0f97afcab0816e394966a59b4b9
Kolovratik <Kolovratik@HOME-PC>
2024-05-27 08:06:49 +0300

    Промежуточное сохранение

---
+++ Data/Interfaces/ICategories.cs
@@ -0,0 +1,10 @@
+﻿using PR37.Data.Models;
+using System.Collections.Generic;
+
+namespace PR37.Data.Interfaces
+{
+    public interface ICategories
+    {
+        public IEnumerable<Categories> AllCategories { get; }
+    }
+}
---
+++ Data/Interfaces/IItems.cs
@@ -0,0 +1,10 @@
+﻿using PR37.Data.Models;
+using System.Collections.Generic;
+
+namespace PR37.Data.Interfaces
+{
+    public interface IItems
+    {
+        public IEnumerable<Items> AllItems { get; }
+    }
+}
