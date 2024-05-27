9019aa0ae044d495f7e66f22a64df23c76cc9420
Kolovratik <Kolovratik@HOME-PC>
2024-05-27 08:04:04 +0300

    Разработанные модели данных.

---
+++ Data/Models/Categories.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace PR37.Data.Models
+{
+    public class Categories
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Items> Items { get; set; }
+    }
+}
---
+++ Data/Models/Items.cs
@@ -0,0 +1,12 @@
+﻿namespace PR37.Data.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Img { get; set; }
+        public int Price { get; set; }
+        public Categoties Category { get; set; }
+    }
+}
