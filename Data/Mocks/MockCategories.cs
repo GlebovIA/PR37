a408ead543ddb5e0ca23ff4041c296cb962e8bb9
student-a202 <student-a202@PC-A202-7>
2024-05-27 12:45:37 +0500

    Разработанные вспомогательные классы с данными.

---
+++ Data/Mocks/MockCategories.cs
@@ -0,0 +1,34 @@
+﻿using PR37.Data.Interfaces;
+using PR37.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PR37.Data.Mocks
+{
+    public class MockCategories : ICategories
+    {
+        public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                return new List<Categories>
+                {
+                    new Categories()
+                    {
+                        Id = 0,
+                        Name = "Микроволновые печи",
+                        Description = "Микроволновки - греют еду."
+                    },
+                    new Categories()
+                    {
+                        Id = 1,
+                        Name = "Телевизоры",
+                        Description = "Телевизоры - смотреть футбольчик."
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Data/Mocks/MockItems.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using PR37.Data.Interfaces;
+using PR37.Data.Models;
+
+public class MockItems : IItems
+{
+	public MockItems()
+    {
+		public ICategories category = new MockCategoties();
+		public IEnumerable<Items> AllItems
+		{
+            get
+            {
+                return new List<Items>()
+                {
+                    new Items()
+                    {
+                        Id = 0,
+                        Name = "DEXP MS-70",
+                        Description = "Абра-кадабра и ваша еда из холодильника готова к употреблению",
+                        Img = "https://c.dns-shop.ru/thumb/st1/fit/300/300/f67706d3e9f41fdc37fbf1d4ea715b35/b1a761fddbd2197e22bdcf5ee0cd1cfd773ce824ab6ef6eba7411b9a626c50a7.jpg.webp"
+                        Price = 7900,
+                        Category = category.AllCategories.Where(x => x.Id == 0).First()
+                    }
+                }
+            }
+		}
+	}
+}
