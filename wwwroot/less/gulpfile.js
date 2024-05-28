0549de4624f4494831e2d7275d6df65902812f51
Kolovratik <Kolovratik@HOME-PC>
2024-05-28 10:25:06 +0300

    Выполненная работа на оценку удовлетворительно

---
+++ wwwroot/less/gulpfile.js
@@ -0,0 +1,13 @@
+﻿"use strict";
+
+var gulp = require("gulp"),
+    less = require("gulp-less");
+
+var paths = {
+    webroot: "./wwwroot/"
+};
+gulp.task("less", function () {
+    return gulp.src('less/styles.less')
+        .pipe(less())
+        .pipe(gulp.dest(paths.webroot + '/css'))
+});
