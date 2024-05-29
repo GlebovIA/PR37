300594f4d5968df1bb4904217f13ee34f8d69053
Kolovratik <Kolovratik@HOME-PC>
2024-05-29 19:27:07 +0300

    Выполненная раббота на оценку отлично

---
+++ Data/less/gulpfile.js
@@ -0,0 +1,15 @@
+﻿var gulp = require('gulp');
+gulp.task('default', function () {
+    var gulp = require('gulp'),
+        less = require('gulp-less');
+
+    gulp.task('less', function () {
+        gulp.src('./Data/less/**/*.less')
+            .pipe(less())
+            .pipe(gulp.dest('./wwwroot/css/Style.css'));
+    });
+
+    gulp.task('default', function () {
+        console.log("Completed");
+    });
+});
