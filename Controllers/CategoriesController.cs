f67bd6227c7920e22e4036e4a8bfe38b933de4b1
Kolovratik <Kolovratik@HOME-PC>
2024-05-29 18:16:28 +0300

    Выполненная работа на оценку хорошо

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PR37.Data.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PR37.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ICategories IAllCategories;
+        public CategoriesController(ICategories IAllCategories)
+        {
+            this.IAllCategories = IAllCategories;
+        }
+        public ViewResult List()
+        {
+            ViewBag.Title = "Страница с категорями";
+            var cars = IAllCategories.AllCategories;
+            return View(cars);
+        }
+    }
+}
