de72edaae29755bfb1041c541c1a7383aba368f0
Kolovratik <Kolovratik@HOME-PC>
2024-05-30 08:07:23 +0300

    Получение данных о предметах.

---
+++ Data/DataBase/DBCategory.cs
@@ -0,0 +1,34 @@
+﻿using PR37.Data.Common;
+using PR37.Data.Interfaces;
+using PR37.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PR37.Data.DataBase
+{
+    public class DBCategory : ICategories
+    {
+        public IEnumerable<Categories> AllCategories
+        {
+            get
+            {
+                List<Categories> categories = new List<Categories>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader CategoriesData = Connection.MySqlQuery("Select * from MyShop.Categories Order By 'Name';", MySqlConnection);
+                while (CategoriesData.Read())
+                {
+                    categories.Add(new Categories()
+                    {
+                        Id = CategoriesData.IsDBNull(0) ? -1 : CategoriesData.GetInt32(0),
+                        Name = CategoriesData.IsDBNull(1) ? null : CategoriesData.GetString(1),
+                        Description = CategoriesData.IsDBNull(2) ? null : CategoriesData.GetString(2)
+                    });
+                }
+                return categories;
+            }
+        }
+    }
+}
---
+++ Data/DataBase/DBItems.cs
@@ -0,0 +1,40 @@
+﻿using PR37.Data.Common;
+using PR37.Data.Interfaces;
+using PR37.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace PR37.Data.DataBase
+{
+    public class DBItems : IItems
+    {
+        public IEnumerable<Categories> Categories = new DBCategory().AllCategories;
+        public IEnumerable<Items> AllItems
+        {
+            get
+            {
+                List<Items> items = new List<Items>();
+                MySqlConnection MySqlConnection = Connection.MySqlOpen();
+                MySqlDataReader ItemsData = Connection.MySqlQuery("Select * from MyShop.Items Order By 'Name';", MySqlConnection);
+                while (ItemsData.Read())
+                {
+                    items.Add(new Items()
+                    {
+                        Id = ItemsData.IsDBNull(0) ? -1 : ItemsData.GetInt32(0),
+                        Name = ItemsData.IsDBNull(1) ? "" : ItemsData.GetString(1),
+                        Description = ItemsData.IsDBNull(2) ? "" : ItemsData.GetString(2),
+                        Img = ItemsData.IsDBNull(3) ? "" : ItemsData.GetString(3),
+                        Price = ItemsData.IsDBNull(4) ? -1 : ItemsData.GetInt32(4),
+                        Category = ItemsData.IsDBNull(5) ? null : Categories.Where(x => x.Id == ItemsData.GetInt32(5)).First()
+                    });
+                }
+                MySqlConnection.Close();
+                
+                return items;
+            }
+        }
+    }
+}
