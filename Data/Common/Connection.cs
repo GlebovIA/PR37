0f49ef76aa26d930bf85dc115cc83969d4e2016d
Kolovratik <Kolovratik@HOME-PC>
2024-05-30 08:03:39 +0300

    Разработанное подключение к MySql.

---
+++ Data/Common/Connection.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace PR37.Data.Common
+{
+    public class Connection
+    {
+        readonly static string ConnectionData = "server=127.0.0.1;port=3306;database=MyShop;uid=root;pwd=;";
+        public static MySqlConnection MySqlOpen()
+        {
+            MySqlConnection NewMySqlConnection = new MySqlConnection(ConnectionData);
+            NewMySqlConnection.Open();
+            return NewMySqlConnection;
+        }
+        public static MySqlDataReader MySqlQuery(string query, MySqlConnection connection)
+        {
+            MySqlCommand NewMySqlCommand = new MySqlCommand(query, connection);
+            return NewMySqlCommand.ExecuteReader();
+        }
+        public static void MySqlClose(MySqlConnection connection)
+        {
+            connection.Close();
+        }
+    }
+}
